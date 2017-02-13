7206ce3aa0e51c37be90294e580c935def6ef1f4
SushobhanAgrawal <sa929@njit.edu>
2017-02-13 03:11:46 -0500

    rectified date

--- display_results.php
+++ display_results.php
@@ -69,7 +69,7 @@
 
         <label>Future Value:</label>
         <span><?php echo $future_value_f; ?></span><br>
-	<h6> echo date("m/d/y");</h6>
+	<?php <h6> echo date("m/d/y");</h6> ?>
     </main>
 </body>
 </html>
